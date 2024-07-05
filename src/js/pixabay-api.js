95f9517e01b178bc93594a34fbd16734748135e1
Dmitro <kt040880bds2@gmail.com>
2024-07-05 21:14:43 +0300

    05/07

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,17 @@
+const URL = 'https://pixabay.com/api';
+const API_KEY = '44774560-6cc9e3fc3beea571d43e1a675';
+
+function searchImages(str) {
+  return fetch(
+    `${URL}?key=${API_KEY}&q=${encodeURIComponent(
+      str
+    )}&image_type=photo&orientation=horizontal&safesearch=true`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
+
+export { searchImages };
